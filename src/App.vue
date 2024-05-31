32a7d5718007ee9af6fb4124852770f5dac26dc6
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-05-31 16:48:56 +0200

    [add] new project Vue

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+  export default {
+    data(){
+      return{
+
+      }
+    },
+
+    methods:{
+
+    },
+
+    mounted(){
+
+    }
+  }
+</script>
+
+
+<template>
+  <div>
+    <p>prova</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
