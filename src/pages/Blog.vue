3d7c18bf65ea598a675a869ae1d6da621ac77038
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-06-03 21:13:39 +0200

    [add] getApi: technologies and types. Error!

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -13,29 +13,51 @@ export default {
   data() {
     return {
       projects: [],
+      technologies: [],
+      myTypes: [],
       paginatorData: {},
     }
   },
 
   methods: {
-    getApi(apiUrl) {
+    getApi(apiUrl, type = '') {
+      // perchè non stampa technologies e types? e perchè se passo il parametro type mi da errore?
       axios.get(apiUrl)
         .then(result => {
-          this.projects = result.data.data
+
+          switch (type) {
+            case 'technologies':
+              this.technologies = result.data
+              console.log(this.technologies)
+
+              break;
+            case 'types':
+              // this.types = result.data
+              this.myTypes = result.data
+              console.log(this.myTypes)
+
+            default:
+              this.projects = result.data.data
+              // paginator
+              this.paginatorData.current_page = result.data.current_page;
+              this.paginatorData.links = result.data.links;
+              this.paginatorData.total = result.data.total;
+              break;
+          }
+
           console.log(result.data)
-          // paginator
-          this.paginatorData.current_page = result.data.current_page;
-          this.paginatorData.links = result.data.links;
-          this.paginatorData.total = result.data.total;
+
           console.log(this.paginatorData)
         })
         .catch(error => {
-          console.log(error.message);
+          console.log(error.message, 'projects');
         })
     }
   },
   mounted() {
-    this.getApi(store.apiUrl);
+    this.getApi(store.apiUrl, 'projects');
+    this.getApi(store.apiUrl, 'types');
+    this.getApi(store.apiUrl, 'technologies');
   }
 }
 
@@ -52,18 +74,16 @@ export default {
       </ul>
 
       <div>
-        <h4>Tags:</h4>
+        <h4>Types:</h4>
         <div>
-          <span class="me-2 badge text-bg-primary">tag</span>
-          <span class="me-2 badge text-bg-primary">tag</span>
+          <span v-for="myType in myTypes" :key="`x-${myType.id}`" class="me-2 badge text-bg-primary">{{ myType.title }}</span>
         </div>
       </div>
 
       <div>
         <h4>Technology:</h4>
         <div>
-          <span class="me-2 badge text-bg-primary">technology</span>
-          <span class="me-2 badge text-bg-primary">technology</span>
+          <span v-for="Technology in technologies" :key="`x-${technologies.id}`" class="me-2 badge text-bg-primary">{{ technology.title }}</span>
         </div>
       </div>
 
